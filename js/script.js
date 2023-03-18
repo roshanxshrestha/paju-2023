4274fb2a4146be5b8fb15b280848ac3054e2af7b
Roshan Shrestha <85292792+roshanxshrestha@users.noreply.github.com>
2023-03-18 22:20:42 +0545

    aos

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ window.addEventListener("load", function () {
     document.querySelector(".page-loader").style.display = "none";
   }, 600);
   /*------------------animation on scroll------------------------*/
-  AOS.init();
+  AOS.init({
+ once: true,
+});
+  
 });
 /*--------------------------------toggle navbar---------------------*/
 
